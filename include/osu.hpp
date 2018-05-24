591461d7a38ed36b7b78dff1c5efcdcc00725984
Michael Zhang <failed.down@gmail.com>
2018-05-24 15:43:12 -0500

    includeall

---
+++ include/osu.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef OSU_HPP_
+#define OSU_HPP_
+
+#include "osu/beatmap/event.hpp"
+#include "osu/beatmap/hitobject.hpp"
+#include "osu/beatmap/map.hpp"
+#include "osu/beatmap/set.hpp"
+#include "osu/beatmap/timingpoint.hpp"
+
+#include "osu/math/spline.hpp"
+#include "osu/math/vector.hpp"
+
+#include "osu/osz/beatmapparser.hpp"
+
+#include "osu/util/color.hpp"
+#include "osu/util/common.hpp"
+#include "osu/util/enums.hpp"
+#include "osu/util/errors.hpp"
+
+#endif
