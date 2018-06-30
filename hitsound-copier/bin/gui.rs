5115c8207c3df1e00712e125d8a62653fee358c4
Michael Zhang <failed.down@gmail.com>
2018-06-30 15:54:55 -0700

    imported glade gui

--- hitsound-copier/bin/gui.rs
+++ hitsound-copier/bin/gui.rs
@@ -1,4 +1,5 @@
 extern crate failure;
+extern crate gdk;
 extern crate gio;
 extern crate gtk;
 extern crate hitsound_copier;
@@ -6,8 +7,10 @@ extern crate hitsound_copier;
 use std::env;
 
 use failure::Error;
+use gdk::prelude::*;
 use gio::prelude::*;
 use gtk::prelude::*;
+use gtk::Builder;
 
 // from gtk examples
 macro_rules! clone {
@@ -34,14 +37,22 @@ fn main() {
     }
 }
 
-fn build_ui(app: &gtk::Application) {
-    let window = gtk::ApplicationWindow::new(app);
+fn build_ui(app: &gtk::Application) -> Result<(), Error> {
+    let glade_src = include_str!("hscopier.glade");
+    let builder = Builder::new_from_string(glade_src);
+
+    let window: gtk::ApplicationWindow = builder.get_object("window").expect("couldn't get window");
+    window.set_application(app);
+
     window.show_all();
+    Ok(())
 }
 
 fn run() -> Result<(), Error> {
     let application = gtk::Application::new("a.b.c", gio::ApplicationFlags::empty())?;
-    application.connect_startup(|app| build_ui(app));
+    application.connect_startup(|app| {
+        build_ui(app);
+    });
     application.connect_activate(|_| {});
 
     application.run(&env::args().collect::<Vec<_>>());
