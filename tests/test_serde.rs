1cc50d555d6ba39dbc07eeaea5be653e8b674c95
Michael Zhang <mail@mzhang.io>
2020-12-27 01:47:41 -0600

    Seems like older maps didn't require the sample info

--- tests/test_serde.rs
+++ tests/test_serde.rs
@@ -106,6 +106,7 @@ macro_rules! test_serde {
 }
 
 test_serde! {
+    test_parser_129891: 129891,
     test_parser_774965: 774965,
     test_parser_804683: 804683,
     test_parser_1595588: 1595588,
