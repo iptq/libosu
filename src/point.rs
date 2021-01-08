b64739316407ffa9915b5d7f6b477de98986baa2
Michael Zhang <mail@mzhang.io>
2021-01-08 01:13:23 -0600

    add partialeq, eq, and hash implementations for point

--- src/point.rs
+++ src/point.rs
@@ -1,9 +1,28 @@
-use std::fmt::{self, Display};
+use std::fmt::{self, Debug, Display};
+use std::hash::{Hash, Hasher};
 use std::ops::{Add, Mul, Sub};
 
 /// Represents a 2D point (or any pair of objects).
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
 pub struct Point<T>(pub T, pub T);
+
+impl<T: PartialEq> PartialEq for Point<T> {
+    fn eq(&self, other: &Point<T>) -> bool {
+        self.0.eq(&other.0) && self.1.eq(&other.1)
+    }
+}
+
+impl<T: PartialEq + Eq> Eq for Point<T> {}
+
+impl<T: Hash> Hash for Point<T> {
+    fn hash<H>(&self, h: &mut H)
+    where
+        H: Hasher,
+    {
+        self.0.hash(h);
+        self.1.hash(h);
+    }
+}
 
 impl<T: Display> fmt::Display for Point<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
